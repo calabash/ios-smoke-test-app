96ae67eb3678a80a30d2afe72d4ff8f57cf09b4f
Joshua Moody <joshuajmoody@gmail.com>
2014-08-05 00:48:40 +0200

    removing the meaningless copyright from main.m

--- chou/chou/main.m
+++ chou/chou/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  chou
-//
-//  Created by Joshua Moody on 29.7.14.
-//  Copyright (c) 2014 Xamarin. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 #import "XamAppDelegate.h"
