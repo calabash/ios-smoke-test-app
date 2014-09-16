6079e338b35140d16104a24d20047616f507aa72
Joshua Moody <joshuajmoody@gmail.com>
2014-09-16 13:31:49 +0200

    Pass TRAVIS as env var to system calls

--- script/ci/travis/local-run-as-travis.rb
+++ script/ci/travis/local-run-as-travis.rb
@@ -4,11 +4,15 @@ require File.expand_path(File.join(File.dirname(__FILE__), 'ci-helpers'))
 
 working_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..'))
 
-ENV['TRAVIS'] = '1'
+# noinspection RubyStringKeysInHashInspection
+env_vars =
+      {
+            'TRAVIS' => '1',
+      }
 
 Dir.chdir working_dir do
   uninstall_gem 'calabash-cucumber'
-  do_system('script/ci/travis/bundle-install.rb')
-  do_system('script/ci/travis/build-and-stage-app.sh')
-  do_system('script/ci/travis/cucumber-ci.rb --tags ~@no_ci')
+  do_system('script/ci/travis/bundle-install.rb', {:env_vars => env_vars})
+  do_system('script/ci/travis/build-and-stage-app.sh', {:env_vars => env_vars})
+  do_system('script/ci/travis/cucumber-ci.rb --tags ~@no_ci', {:env_vars => env_vars})
 end
