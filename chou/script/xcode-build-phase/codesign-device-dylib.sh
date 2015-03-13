e00e5e3988b0e709c57916ecc56c35d734124be4
Joshua Moody <joshuajmoody@gmail.com>
2015-03-13 19:03:45 +0100

    Add device dylib codesign Run Script build phase

---
+++ chou/script/xcode-build-phase/codesign-device-dylib.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# If you see an error like this:
+#
+# iPhone Developer: ambiguous (matches "iPhone Developer: Person A (2<snip>Q)"
+#                                  and "iPhone Developer: Person B (8<snip>F)"
+# in /Users/<snip>/Library/Keychains/login.keychain)
+#
+# Uncomment this line and update it with the correct credentials.
+# CODE_SIGN_IDENTITY="iPhone Developer: Person B (8<snip>F)"
+
+set -e
+
+if [ -n "${CODE_SIGN_IDENTITY}" ]; then
+    xcrun codesign -fs "${CODE_SIGN_IDENTITY}" "${BUILT_PRODUCTS_DIR}/${FULL_PRODUCT_NAME}/libCalabashDyn.dylib"
+fi
+
