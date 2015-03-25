6574aae720a0ea7d3cbc9461f2f4786ea5651368
Joshua Moody <joshuajmoody@gmail.com>
2015-03-25 21:46:29 +0100

    Replace instruments auth w/ 'luffa authorize'

--- script/ci/travis/local-run-as-travis.rb
+++ script/ci/travis/local-run-as-travis.rb
@@ -12,6 +12,11 @@ env_vars =
 Dir.chdir working_dir do
   Luffa::Gem.uninstall_gem 'calabash-cucumber'
   Luffa.unix_command('script/ci/travis/bundle-install.rb', {:env_vars => env_vars})
+  Dir.chdir 'CalSmokeApp' do
+    Luffa.unix_command('bundle exec luffa authorize',
+                       {:pass_msg => 'Successfully authorized instruments',
+                        :fail_msg => 'Could not authorize instruments'})
+  end
   Luffa.unix_command('script/ci/travis/build-and-stage-app.sh', {:env_vars => env_vars})
   Luffa.unix_command('script/ci/travis/cucumber-ci.rb --tags ~@no_ci', {:env_vars => env_vars})
 end
