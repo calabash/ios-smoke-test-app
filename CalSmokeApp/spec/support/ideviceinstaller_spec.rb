92277fefe011026fe9d97ce3edd436dcc5abe85d
Joshua Moody <joshuajmoody@gmail.com>
2015-05-07 23:13:33 +0200

    Add rspec testing stack

---
+++ CalSmokeApp/spec/support/ideviceinstaller_spec.rb
@@ -0,0 +1,11 @@
+describe Calabash::IPA do
+  it 'has a behavior' do
+    expect(1).to be == 1
+  end
+end
+
+describe Calabash::IDeviceInstaller do
+  it 'has a behavior' do
+    expect(1).to be == 2
+  end
+end
