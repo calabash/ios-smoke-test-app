744c546720a9b224ecc14c56f294a27351893aea
Joshua Moody <joshuajmoody@gmail.com>
2015-05-13 21:22:15 +0200

    Add xtc-other-gems

---
+++ CalSmokeApp/config/xtc-other-gems.rb
@@ -0,0 +1,22 @@
+gem 'run_loop', '>= 1.3.3', '< 2.0'
+gem 'retriable', '~> 2.0'
+
+# workflow and build tools
+gem 'rake', '~> 10.3'
+gem 'bundler', '~> 1.6'
+gem 'xcpretty', '~> 0.1'
+
+# rspec stack
+gem 'rspec', '~> 3.0'
+gem 'guard-rspec', '~> 4.3'
+gem 'guard-bundler', '~> 2.0'
+gem 'growl', '~> 1.0'
+gem 'stub_env', '>= 1.0.1', '< 2.0'
+
+# pry
+gem 'pry'
+gem 'pry-nav'
+
+# CI Stack
+gem 'travis', '~> 1.7'
+gem 'luffa', '~> 1.0', '>= 1.0.2'
