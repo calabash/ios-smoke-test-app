566f6c4c557cbc6d03cdf75a0a80b1b2e442e262
Joshua Moody <joshuajmoody@gmail.com>
2015-06-25 21:15:40 +0200

    Scenario for turning auto-caps off

---
+++ CalSmokeApp/features/step_definitions/keyboard_steps.rb
@@ -0,0 +1,47 @@
+module CalSmokeApp
+  module Keyboard
+
+    CAPITALIZATION =
+          {
+           none: 0,      # UITextAutocapitalizationTypeNone
+           words: 1,     # UITextAutocapitalizationTypeWords
+           sentences: 2, # UITextAutocapitalizationTypeSentences
+           all: 3        # UITextAutocapitalizationTypeAllCharacters
+          }
+
+    def auto_capitalization_type
+      query('UITextField', :autocapitalizationType).first
+    end
+
+    def set_auto_capitalization_type(name)
+      if keyboard_visible?
+        fail('Cannot set the capitalization type if the keyboard is visible')
+      end
+
+      type = CAPITALIZATION[name]
+
+      unless type
+        raise "Unknown capitalization type: '#{name}'. Valid names: :none, :words, :sentences, :all"
+      end
+
+      query('UITextField index:0', [{setAutocapitalizationType: type}])
+    end
+  end
+end
+
+World(CalSmokeApp::Keyboard)
+
+And(/^I turn off auto capitalization$/) do
+  set_auto_capitalization_type :none
+end
+
+Then(/^I touch the text field$/) do
+  touch('UITextField')
+  wait_for_keyboard
+end
+
+When(/^I type "([^"]*)", I should see "([^"]*)"$/) do |typed, expected|
+  keyboard_enter_text typed
+  actual = query('UITextField', :text).first
+  expect(actual).to be == expected
+end
