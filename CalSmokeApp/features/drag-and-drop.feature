d0398b1bb2d161fdaca6c82a6b4bd8cc72430543
Joshua Moody <joshuajmoody@gmail.com>
2015-06-25 03:21:39 +0200

    Remove the verification Scenario

--- CalSmokeApp/features/drag-and-drop.feature
+++ CalSmokeApp/features/drag-and-drop.feature
@@ -7,13 +7,6 @@ Feature: Drag and Drop
   Background:  Get me to the third tab
    Given I see the third tab
 
-  Scenario: Verify layout
-    Then I expect to see the red box
-    Then I expect to see the blue box
-    Then I expect to see the green box
-    Then I expect to see a well on the left
-    Then I expect to see a well on the right
-
   Scenario: Drag the red box
     When I drag the red box to the left well
     Then the left well should change to red
--- CalSmokeApp/features/step_definitions/drag_and_drop_steps.rb
+++ CalSmokeApp/features/step_definitions/drag_and_drop_steps.rb
@@ -54,38 +54,6 @@ end
 
 World(CalSmokeApp::DragAndDrop)
 
-Then(/^I expect to see the (red|blue|green) box$/) do |color|
-  query = "view marked:'#{color}'"
-  wait_for_elements_exist(query)
-  result = query(query)
-  expect(result.count).to be == 1
-  view = result.first
-  rect = view['rect']
-  expect(rect['width']).to be == 88
-  expect(rect['height']).to be == 88
-  case color
-    when 'red'
-      tag = 3030
-    when 'blue'
-      tag = 3031
-    when 'green'
-      tag = 3032
-  end
-
-  expect(query(query, :tag).first).to be == tag
-end
-
-Then(/^I expect to see a well on the (left|right)$/) do |side|
-  query = "view marked:'#{side} well'"
-  wait_for_elements_exist(query)
-  result = query(query)
-  expect(result.count).to be == 1
-  view = result.first
-  rect = view['rect']
-  expect(rect['width']).to be == 132
-  expect(rect['height']).to be == 132
-end
-
 When(/^I drag the red box to the left well$/) do
   from_query = "UIImageView marked:'red'"
   to_query = "UIView marked:'left well'"
