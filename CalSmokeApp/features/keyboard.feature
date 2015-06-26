c00b72cb5a99dc97fbe354935ae3f33d3017068d
Joshua Moody <joshuajmoody@gmail.com>
2015-06-26 13:44:08 +0200

    Delete spell-check and capitalization Scenarios

--- CalSmokeApp/features/keyboard.feature
+++ CalSmokeApp/features/keyboard.feature
@@ -11,52 +11,14 @@ Feature: Typing on the Keyboard
   Scenario: I should be able to type something
     Then I type "Hello"
 
-  @no
-  Scenario: Turn off auto capitalization
-    And I turn off auto capitalization
-    Then I touch the text field
-    When I type "hello", I should see "hello"
-
-  @no
-  Scenario: Turn on ALL caps
-    And I turn on all caps
-    Then I touch the text field
-    When I type "hello", I should see "HELLO"
-
-  @no
-  Scenario: Turn on cap sentences (default)
-    And I turn on capitalize sentences
-    Then I touch the text field
-    When I type "hello. my name is.", I should see "Hello. My name is."
-
-  @yes
   Scenario: Turn on auto correct
     And I turn on auto correct
     Then I touch the text field
     When I type "exictement" and touch done
     Then the text should be "excitement"
 
-  @yes
   Scenario: Turn off auto correct
     And I turn off auto correct
-    Then I touch the text field
-    When I type "exictement" and touch done
-    Then the text should be "exictement"
-
-  # Turning on spell checking makes no discernible difference.
-  @no
-  Scenario: Turn on spell checking
-    And I turn off auto correct
-    And I turn on spell checking
-    Then I touch the text field
-    When I type "exictement"
-    Then the text should be marked as incorrect
-
-  # Turning on spell checking makes no discernible difference.
-  @no
-  Scenario: Turn off spell checking
-    And I turn off auto correct
-    And I turn off spell checking
     Then I touch the text field
     When I type "exictement" and touch done
     Then the text should be "exictement"
--- CalSmokeApp/features/step_definitions/keyboard_steps.rb
+++ CalSmokeApp/features/step_definitions/keyboard_steps.rb
@@ -1,13 +1,13 @@
 module CalSmokeApp
   module Keyboard
-
-    CAPITALIZATION =
-          {
-           none: 0,      # UITextAutocapitalizationTypeNone
-           words: 1,     # UITextAutocapitalizationTypeWords
-           sentences: 2, # UITextAutocapitalizationTypeSentences
-           all: 3        # UITextAutocapitalizationTypeAllCharacters
-          }
+    # For a comprehensive investigation of setting the capitalization,
+    # auto-correct, and spell-check properites of a UITextField see this gist:
+    #
+    # https://gist.github.com/jmoody/106d56a73beff1ffc291
+    #
+    # TL;DR
+    #
+    # You can turn auto-correct on or off _before_ the keyboard is presented.
 
     CORRECTION =
           {
@@ -16,36 +16,15 @@ module CalSmokeApp
                 on: 2       # UITextAutocorrectionTypeYes
           }
 
-    SPELL_CHECKING =
-          {
-                default: 0, # UITextSpellCheckTypeDefault,
-                off: 1,     # UITextSpellCheckTypeNo,
-                on: 2       # UITextSpellCheckTypeYes
-          }
-
-    def auto_capitalization_type
-      query('UITextField', :autocapitalizationType).first
-    end
-
-    def set_auto_capitalization_type(name)
-      if keyboard_visible?
-        fail('Cannot set the capitalization type if the keyboard is visible')
-      end
-
-      type = CAPITALIZATION[name]
-
-      unless type
-        raise "Unknown capitalization type: '#{name}'. Valid names: :none, :words, :sentences, :all"
-      end
-
-      query('UITextField', [{setAutocapitalizationType:type}])
-    end
-
     def auto_correct_type
       query('UITextField', :autocorrectionType).first
     end
 
     def set_auto_correct_type(name)
+      if keyboard_visible?
+        fail('Cannot change the auto-correct settings if the keyboard is visible')
+      end
+
       type = CORRECTION[name]
 
       unless type
@@ -54,58 +33,18 @@ module CalSmokeApp
 
       query('UITextField', [{setAutocorrectionType:type}])
     end
-
-    def spell_check_type
-      query('UITextField', :spellCheckingType).first
-    end
-
-    def set_spell_check_type(name)
-      type = SPELL_CHECKING[name]
-
-      unless type
-        raise "Unknown spell check type: '#{name}'. Valid names: :default, :no, :yes"
-      end
-
-      query('UITextField', [{setSpellCheckingType:type}])
-    end
   end
 end
 
 World(CalSmokeApp::Keyboard)
 
-And(/^I turn off auto capitalization$/) do
-  set_auto_capitalization_type :none
-end
-
-And(/^I turn on all caps$/) do
-  set_auto_capitalization_type :all
-end
-
-And(/^I turn on capitalize sentences$/) do
-  set_auto_capitalization_type :words
-end
-
 And(/^I turn (on|off) auto correct$/) do |on_or_off|
   set_auto_correct_type(on_or_off.to_sym)
-end
-
-And(/^I turn (on|off) spell checking$/) do |on_or_off|
-  set_spell_check_type(on_or_off.to_sym)
-end
-
-Then(/^the text should be marked as incorrect$/) do
-  pending
 end
 
 Then(/^I touch the text field$/) do
   touch('UITextField')
   wait_for_keyboard
-end
-
-When(/^I type "([^"]*)", I should see "([^"]*)"$/) do |typed, expected|
-  keyboard_enter_text typed
-  actual = query('UITextField', :text).first
-  expect(actual).to be == expected
 end
 
 When(/^I type "([^"]*)" and touch done$/) do |typed|
