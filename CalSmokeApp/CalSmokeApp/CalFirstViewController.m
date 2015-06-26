5580c3720edbf63ef4a7c2b7add2da9d9a569ffa
Joshua Moody <joshuajmoody@gmail.com>
2015-06-26 14:37:35 +0200

    Log the UITextInput trait enums

--- CalSmokeApp/CalSmokeApp/CalFirstViewController.m
+++ CalSmokeApp/CalSmokeApp/CalFirstViewController.m
@@ -187,6 +187,22 @@ typedef enum : NSInteger {
   BOOL lastSwitchState = [defaults boolForKey:kUserDefaultsSwitchState];
   CalFirstView *view = (CalFirstView *)self.view;
   [view.uiswitch setOn:lastSwitchState];
+
+  NSLog(@"Text input Capitalization Types");
+  NSLog(@"     none = %@", @(UITextAutocapitalizationTypeNone));
+  NSLog(@"    words = %@", @(UITextAutocapitalizationTypeWords));
+  NSLog(@"sentences = %@", @(UITextAutocapitalizationTypeSentences));
+  NSLog(@"      all = %@", @(UITextAutocapitalizationTypeAllCharacters));
+
+  NSLog(@"Text input Correction Types");
+  NSLog(@"  default = %@", @(UITextAutocorrectionTypeDefault));
+  NSLog(@"       no = %@", @(UITextAutocorrectionTypeNo));
+  NSLog(@"      yes = %@", @(UITextAutocorrectionTypeYes));
+
+  NSLog(@"Text input Spell Checking Types");
+  NSLog(@"  default = %@", @(UITextSpellCheckingTypeDefault));
+  NSLog(@"       no = %@", @(UITextSpellCheckingTypeNo));
+  NSLog(@"      yes = %@", @(UITextSpellCheckingTypeYes));
   [super viewWillAppear:animated];
 }
 
