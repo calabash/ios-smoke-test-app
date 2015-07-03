c2f8069fb934538785fa027367fdb8344aac4465
Joshua Moody <joshuajmoody@gmail.com>
2015-07-03 14:40:55 +0200

    Add animation Scenarios to test server/#142
    https://github.com/calabash/calabash-ios-server/pull/142

--- CalSmokeApp/features/step_definitions/animations_steps.rb
+++ CalSmokeApp/features/step_definitions/animations_steps.rb
@@ -8,3 +8,16 @@ Then(/^I wait for all animations to stop$/) do
   # When checking for no animation, ignore animations with trivially short durations
   wait_for_none_animating(options)
 end
+
+And(/^I have started an animation that lasts (\d+) seconds$/) do |duration|
+  @last_animation_duration = duration.to_i
+  backdoor('animateOrangeViewOnDragAndDropController:', duration)
+end
+
+Then(/^I can wait for the animation to stop$/) do
+  timeout = @last_animation_duration + 1
+  message = "Waited #{timeout} seconds for the animation to stop"
+  options = {timeout: timeout, timeout_message: message}
+
+  wait_for_none_animating(options)
+end
