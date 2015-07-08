439f3c1a974f2870f43725ba9355bb9827a28e0d
Joshua Moody <joshuajmoody@gmail.com>
2015-07-08 16:41:39 +0200

    Add selectors that return values

--- CalSmokeApp/CalSmokeApp/CalViewWithArbitrarySelectors.m
+++ CalSmokeApp/CalSmokeApp/CalViewWithArbitrarySelectors.m
@@ -21,9 +21,29 @@
 - (BOOL) takesUnsignedLong:(unsigned long) arg;
 - (BOOL) takesLongLong:(long long) arg;
 - (BOOL) takesUnsignedLongLong:(unsigned long long) arg;
-
 - (BOOL) takesPoint:(CGPoint) arg;
 - (BOOL) takesRect:(CGRect) arg;
+
+- (void) returnsVoid;
+- (NSString *) returnsPointer;
+- (char) returnsChar;
+- (unichar) returnsUnsignedChar;
+- (char *) returnsCString;
+- (BOOL) returnsBOOL;
+- (bool) returnsBool;
+- (NSInteger) returnsInt;
+- (NSUInteger) returnsUnsignedInt;
+- (short) returnsShort;
+- (unsigned short) returnsUnsignedShort;
+- (double) returnsDouble;
+- (long double) returnsLongDouble;
+- (float) returnsFloat;
+- (long) returnsLong;
+- (unsigned long) returnsUnsignedLong;
+- (long long) returnsLongLong;
+- (unsigned long long) returnsUnsignedLongLong;
+- (CGPoint) returnsPoint;
+- (CGRect) returnsRect;
 
 @end
 
@@ -45,8 +65,28 @@
 - (BOOL) takesUnsignedLong:(unsigned long) arg { return YES; }
 - (BOOL) takesLongLong:(long long) arg { return YES; }
 - (BOOL) takesUnsignedLongLong:(unsigned long long) arg { return YES; }
-
 - (BOOL) takesPoint:(CGPoint) arg { return YES; }
 - (BOOL) takesRect:(CGRect) arg { return YES; }
+
+- (void) returnsVoid { return; }
+- (NSString *) returnsPointer { return @"a pointer"; }
+- (char) returnsChar { return -22; }
+- (unichar) returnsUnsignedChar { return 'a'; }
+- (char *) returnsCString { return "c string"; }
+- (BOOL) returnsBOOL { return YES; }
+- (bool) returnsBool { return true; }
+- (NSInteger) returnsInt { return -3; }
+- (NSUInteger) returnsUnsignedInt { return 3; }
+- (short) returnsShort { return -2; }
+- (unsigned short) returnsUnsignedShort { return 2; }
+- (double) returnsDouble { return 0.5; }
+- (long double) returnsLongDouble { return (long double)0.55; }
+- (float) returnsFloat { return 3.14f; }
+- (long) returnsLong { return (long)-4; }
+- (unsigned long) returnsUnsignedLong { return (unsigned long)4; }
+- (long long) returnsLongLong { return (long long)-5; }
+- (unsigned long long) returnsUnsignedLongLong { return (unsigned long long)5; }
+- (CGPoint) returnsPoint { return CGPointZero; }
+- (CGRect) returnsRect { return CGRectZero; }
 
 @end
