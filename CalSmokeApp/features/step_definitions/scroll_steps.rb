400c4b8d0df29c52261d479a49a264727c5626c6
Joshua Moody <joshuajmoody@gmail.com>
2015-07-25 12:25:10 +0200

    Add a wait_options helper modulure

--- CalSmokeApp/features/step_definitions/scroll_steps.rb
+++ CalSmokeApp/features/step_definitions/scroll_steps.rb
@@ -1,24 +1,27 @@
 
 Then(/^I see the scrolling views table$/) do
   query = "UITableView marked:'table'"
-  wait_for_element_exists(query)
+  options = wait_options('Scrolling views table')
+  wait_for_element_exists(query, options)
 end
 
 And(/^I see rows for table, collection, scroll, web, and map views$/) do
   queries = ['table', 'collection', 'scroll', 'web', 'map'].map do |name|
     "UITableViewCell marked:'#{name} views row'"
   end
-
-  wait_for_elements_exist(queries)
+  options = wait_options('Scrolling views rows')
+  wait_for_elements_exist(queries, options)
 end
 
 When(/^I rotate to landscape$/) do
   rotate_home_button_to 'right'
+  wait_for_none_animating
 end
 
 When(/^I touch the collection views row$/) do
   query = "UITableViewCell marked:'collection views row'"
-  wait_for_elements_exist(query)
+  options = wait_options('Collection views row')
+  wait_for_elements_exist(query, options)
 
   touch(query)
   wait_for_none_animating
@@ -26,5 +29,6 @@ end
 
 Then(/^I see the collection view page$/) do
   query = "view marked:'collection view page'"
-  wait_for_elements_exist(query)
+  options = wait_options('Collection view page')
+  wait_for_elements_exist(query, options)
 end
