f7e64c94a10b855d968a9ba16acba836b1748e52
Joshua Moody <joshuajmoody@gmail.com>
2015-08-06 18:23:02 +0200

    Add a date picker tab

---
+++ CalSmokeApp/CalSmokeApp/CalDatePickerController.m
@@ -0,0 +1,199 @@
+#import "CalDatePickerController.h"
+#import "CalDatePickerView.h"
+
+
+typedef enum : NSInteger {
+  kTagPickerView = 3030,
+  kTagButtonTime,
+  kTagButtonDate,
+  kTagButtonDateAndTime
+} view_tags;
+
+
+
+@interface CalDatePickerController ()
+
+@property (nonatomic, strong) CalDatePickerView *pickerView;
+
+- (void) buttonTouchedDoneDatePicking:(id) sender;
+- (void) setButtonsHidden:(BOOL) aHidden;
+- (UIDatePickerMode) modeForButton:(UIButton *) aButton;
+- (void) buttonTouched:(UIButton *) aButton;
+- (CalDatePickerView *) pickerViewForMode:(UIDatePickerMode) aMode;
+
+@end
+
+@implementation CalDatePickerController
+
+@synthesize pickerView = _pickerView;
+
+- (id) initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+  self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+  if (self) {
+
+    UIImage *image = [UIImage imageNamed:@"tab-bar-elephant"];
+    UIImage *selected = [UIImage imageNamed:@"tab-bar-elephant-selected"];
+    NSString *title = NSLocalizedString(@"Date Picker",
+                                        @"Title of tab with date pickers.");
+    self.tabBarItem = [[UITabBarItem alloc]
+                       initWithTitle:title
+                       image:image
+                       selectedImage:selected];
+  }
+  return self;
+}
+
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+
+- (CalDatePickerView *) pickerView {
+  return _pickerView;
+}
+
+- (CalDatePickerView *) pickerViewForMode:(UIDatePickerMode) aMode {
+  return [[CalDatePickerView alloc]
+                 initWithDate:[NSDate date]
+                 delegate:self
+                 mode:aMode];
+}
+
+#pragma mark - Actions
+
+- (void) buttonTouchedDoneDatePicking:(id)sender {
+  NSLog(@"done date editing button touched");
+}
+
+
+- (void) setButtonsHidden:(BOOL) aHidden {
+  NSArray *buttons = @[self.buttonTime, self.buttonDate, self.buttonDateAndTime];
+  [buttons enumerateObjectsUsingBlock:^(UIButton *button,
+                                        NSUInteger idx,
+                                        BOOL *stop) {
+    button.hidden = aHidden;
+  }];
+}
+
+- (UIDatePickerMode) modeForButton:(UIButton *) aButton {
+  if (self.buttonTime == aButton) { return UIDatePickerModeTime; }
+  if (self.buttonDate == aButton) { return UIDatePickerModeDate; }
+  if (self.buttonDateAndTime == aButton) { return UIDatePickerModeDateAndTime; }
+  return NSNotFound;
+}
+
+- (void) buttonTouched:(UIButton *) aButton {
+  UIDatePickerMode mode = [self modeForButton:aButton];
+  _pickerView = [self pickerViewForMode:mode];
+  
+  [self setButtonsHidden:YES];
+  
+  typeof(self) wself = self;
+  [CalDatePickerAnimationHelper
+   animateDatePickerOnWithController:wself
+   animations:^{
+     
+   } completion:^(BOOL finished) {
+    
+   }];
+}
+
+- (IBAction)buttonTouchedTime:(id)sender {
+  NSLog(@"show picker button touched");
+  [self buttonTouched:sender];
+}
+
+- (IBAction)buttonTouchedDateAndTime:(id)sender {
+  NSLog(@"show picker button touched");
+  [self buttonTouched:sender];
+}
+
+- (IBAction)buttonTouchedDate:(id)sender {
+  NSLog(@"show picker button touched");
+  [self buttonTouched:sender];
+}
+
+#pragma mark - <CalDatePickerViewDelegate>
+
+- (void) datePickerViewCancelButtonTouched {
+  NSLog(@"cancel date picking button touched");
+  typeof(self) wself = self;
+  [CalDatePickerAnimationHelper
+   animateDatePickerOffWithController:wself
+   before:^{
+     
+   } animations:^{
+     
+   } completion:^(BOOL finished) {
+     [wself setButtonsHidden:NO];
+   }];
+}
+
+- (void) datePickerViewDoneButtonTouchedWithDate:(NSDate *)aDate {
+  NSLog(@"picker view done button touched with date: %@",  [aDate debugDescription]);
+  
+  typeof(self) wself = self;
+  [CalDatePickerAnimationHelper
+   animateDatePickerOffWithController:wself
+   before:^{
+
+   } animations:^{
+
+   } completion:^(BOOL finished) {
+     [wself setButtonsHidden:NO];
+   }];
+}
+
+
+#pragma mark - Orientation
+
+- (NSUInteger) supportedInterfaceOrientations {
+  return UIInterfaceOrientationMaskPortrait;
+}
+
+- (BOOL) shouldAutorotate {
+  return NO;
+}
+
+#pragma mark - View Lifecycle
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+
+  self.view.accessibilityIdentifier = @"date related";
+  self.buttonTime.accessibilityIdentifier = @"show time picker";
+  self.buttonTime.tag = kTagButtonTime;
+  self.buttonDate.accessibilityIdentifier = @"show date picker";
+  self.buttonDate.tag = kTagButtonDate;
+  self.buttonDateAndTime.accessibilityIdentifier = @"show date and time picker";
+  self.buttonDateAndTime.tag = kTagButtonDateAndTime;
+}
+
+- (void) viewWillLayoutSubviews {
+  [super viewWillLayoutSubviews];
+}
+
+- (void) viewDidLayoutSubviews {
+  [super viewDidLayoutSubviews];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+  [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+  [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+  [super viewWillDisappear:animated];
+  [self datePickerViewCancelButtonTouched];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+  [super viewDidDisappear:animated];
+}
+
+@end
---
+++ CalSmokeApp/CalSmokeApp/Views/CalDatePickerView.m
@@ -0,0 +1,355 @@
+#if ! __has_feature(objc_arc)
+#warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
+#endif
+
+#import "CalDatePickerView.h"
+#import "UIView+Positioning.h"
+
+@interface NSLocale (CalAdditions)
+
+- (BOOL) localeUses24HourClock;
+
+@end
+
+@interface NSCalendar (CalAdditions)
+
++ (NSCalendar *) gregorianCalendarWithMondayAsFirstDayOfWeek;
+
+@end
+
+@implementation NSLocale (CalAdditions)
+
+- (BOOL) localeUses24HourClock {
+  NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+  [formatter setLocale:self];
+  [formatter setDateStyle:NSDateFormatterNoStyle];
+  [formatter setTimeStyle:NSDateFormatterShortStyle];
+  NSString *dateString = [formatter stringFromDate:[NSDate date]];
+  NSRange amRange = [dateString rangeOfString:[formatter AMSymbol]];
+  NSRange pmRange = [dateString rangeOfString:[formatter PMSymbol]];
+  BOOL is24Hour = (amRange.location == NSNotFound && pmRange.location == NSNotFound);
+  return is24Hour;
+}
+
+@end
+
+@implementation NSCalendar (CalAdditions)
+
++ (NSCalendar *) gregorianCalendarWithMondayAsFirstDayOfWeek {
+  NSCalendar *calendar;
+  calendar = [[NSCalendar alloc]
+              initWithCalendarIdentifier:NSGregorianCalendar];
+  // iso spec http://en.wikipedia.org/wiki/ISO_week_date#First_week
+  // not respected in iOS 5.0 so we set it here
+  [calendar setMinimumDaysInFirstWeek:4];
+  // monday - days are 1 indexed
+  [calendar setFirstWeekday:2];
+  return calendar;
+}
+
+@end
+
+#pragma mark - Animations
+
+@implementation CalDatePickerAnimationHelper
+
++ (CGFloat) maxY {
+  return [[UIScreen mainScreen] bounds].size.height;
+}
++ (void) configureNavbarForDateEditingWithAnimation:(BOOL)animated
+                                         controller:(UIViewController<CalDatePickerViewDelegate> *)aController {
+  [aController.navigationItem setRightBarButtonItem:nil animated:NO];
+  UIBarButtonItem *cancel = [[UIBarButtonItem alloc]
+                             initWithBarButtonSystemItem:UIBarButtonSystemItemCancel
+                             target:aController
+                             action:@selector(datePickerViewCancelButtonTouched)];
+  cancel.accessibilityLabel = NSLocalizedString(@"cancel date picking",
+                                                @"date: ACCESSIBILITY navbar/toolbar button - touching cancels date picking, rolling back any changes.");
+  [aController.navigationItem setLeftBarButtonItem:cancel animated:animated];
+}
+
+
++ (void) animateDatePickerOnWithController:(UIViewController<CalDatePickerViewDelegate> *) aController
+                                    animations:(void (^)(void)) aAnimations
+                                    completion:(void (^)(BOOL finished)) aCompletion {
+  CalDatePickerView *pickerView = [aController pickerView];
+  pickerView.alpha = 0.0;
+  [aController.view addSubview:pickerView];
+  pickerView.y = [CalDatePickerAnimationHelper maxY];
+  pickerView.alpha = 1.0;
+  
+  CGFloat pickerY = 64;
+  if (aController.hidesBottomBarWhenPushed == YES) { pickerY += 49; }
+  //if (br_is_4in_iphone()) { pickerY += 568 - 480; }
+  
+  __weak UIViewController *wCon = aController;
+  [UIView animateWithDuration:0.4
+                        delay:0.1
+                      options:UIViewAnimationOptionCurveEaseIn
+                   animations:^{
+                     aAnimations();
+                     pickerView.y = pickerY;
+                   }
+                   completion:^(BOOL completed) {
+                     aCompletion(completed);
+                     [wCon.navigationItem setRightBarButtonItem:nil animated:NO];
+                     [CalDatePickerAnimationHelper configureNavbarForDateEditingWithAnimation:YES
+                                                                                      controller:aController];
+                   }];
+}
+
+
+
++ (void) animateDatePickerOffWithController:(UIViewController<CalDatePickerViewDelegate> *) aController
+                                         before:(void (^)(void)) aBefore
+                                     animations:(void (^)(void)) aAnimations
+                                     completion:(void (^)(BOOL finished)) aCompletion {
+  aBefore();
+  
+  CGFloat targetY = [CalDatePickerAnimationHelper maxY];
+  CalDatePickerView *pickerView = [aController pickerView];
+  [UIView animateWithDuration:0.4
+                        delay:0.1
+                      options:UIViewAnimationOptionCurveEaseIn
+                   animations:^{
+                     pickerView.y = targetY;
+                     aAnimations();
+                   }
+                   completion:^(BOOL completed) {
+                     [aController.navigationItem setLeftBarButtonItem:nil animated:YES];
+                     [pickerView removeFromSuperview];
+                     aCompletion(completed);
+                   }];
+}
+
+@end
+
+#pragma mark - Date Picker View
+
+typedef enum : NSInteger {
+  kTagLabel = 3030,
+  kTagToolbar,
+  kTagPicker
+} view_tags;
+
+static NSString *const k_24H_time_format = @"H:mm";
+static NSString *const k_12H_time_format = @"h:mm a";
+static NSString *const k_24H_date_format = @"EEE d MMM";
+static NSString *const k_12H_date_format = @"EEE MMM d";
+
+@interface CalDatePickerView ()
+
+@property (nonatomic, copy) NSDate *date;
+@property (nonatomic, weak) id<CalDatePickerViewDelegate> delegate;
+@property (nonatomic, strong, readonly) UILabel *label;
+@property (nonatomic, strong, readonly) UIDatePicker *picker;
+@property (nonatomic, assign) UIDatePickerMode pickerMode;
+@property (nonatomic, strong, readonly) UIToolbar *toolbar;
+
+- (void) datePickerDateDidChange:(id) sender;
+- (void) buttonTouchedPickerDone:(id) sender;
+
+
+- (NSString *) accessibilityIdentifierForMode:(UIDatePickerMode) aMode;
+- (NSString *) stringForDate:(NSDate *)aDate withMode:(UIDatePickerMode) aMode;
+- (NSString *) accessibilityIdentifierForPickerWithMode:(UIDatePickerMode) aMode;
+
+@end
+
+@implementation CalDatePickerView
+
+@synthesize label = _label;
+@synthesize picker = _picker;
+@synthesize toolbar = _toolbar;
+
+#pragma mark Memory Management
+
+- (id) init {
+ // [self doesNotRecognizeSelector:_cmd];
+  return nil;
+}
+
+- (id) initWithFrame:(CGRect)frame {
+ // [self doesNotRecognizeSelector:_cmd];
+  return nil;
+}
+
+- (id) initWithDate:(NSDate *) aDate
+           delegate:(id<CalDatePickerViewDelegate>) aDelegate
+               mode:(UIDatePickerMode) aDatePickerMode {
+  CGRect frame = CGRectMake(0, 0, 320 , 367);
+  self = [super initWithFrame:frame];
+  if (self) {
+    self.date = aDate;
+    self.delegate = aDelegate;
+    self.pickerMode = aDatePickerMode;
+    
+    self.accessibilityIdentifier = [self accessibilityIdentifierForMode:aDatePickerMode];
+  }
+  return self;
+}
+
+- (void) layoutSubviews {
+  [super layoutSubviews];
+  
+  if ([self viewWithTag:kTagLabel] == nil) {
+    [self addSubview:[self label]];
+  }
+  
+  if ([self viewWithTag:kTagPicker] == nil) {
+    [self addSubview:[self picker]];
+  }
+  
+  if ([self viewWithTag:kTagToolbar] == nil) {
+    [self addSubview:[self toolbar]];
+  }
+}
+
+
+#pragma mark - Views
+
+- (UILabel *) label {
+  if (_label != nil) { return _label; }
+  CGRect frame = CGRectMake(20, 44, 280, 24);
+  UILabel *result = [[UILabel alloc] initWithFrame:frame];
+  result.textAlignment = NSTextAlignmentCenter;
+  result.lineBreakMode = NSLineBreakByTruncatingMiddle;
+  result.font = [UIFont boldSystemFontOfSize:18];
+  result.text = [self stringForDate:self.date withMode:self.pickerMode];
+  result.accessibilityIdentifier = @"time";
+  result.tag = kTagLabel;
+  _label = result;
+  return result;
+}
+
+- (UIToolbar *) toolbar {
+  if (_toolbar != nil) { return _toolbar; }
+  CGFloat y = 107;
+  CGRect frame = CGRectMake(0, y, 320, 44);
+  UIToolbar *bar = [[UIToolbar alloc] initWithFrame:frame];
+  bar.barStyle =  UIBarStyleDefault;
+  UIBarButtonItem *left = [[UIBarButtonItem alloc]
+                           initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace
+                           target:nil action:nil];
+  UIBarButtonItem *right = [[UIBarButtonItem alloc]
+                            initWithBarButtonSystemItem:UIBarButtonSystemItemDone
+                            target:self action:@selector(buttonTouchedPickerDone:)];
+  
+  right.accessibilityLabel = NSLocalizedString(@"done picking date",
+                                               @"date: ACCESSIBILITY button on toolbar/navbar - touching ends date picking and saves any changes");
+  
+  bar.items = @[left,right];
+  bar.tag = kTagToolbar;
+  _toolbar = bar;
+  return bar;
+}
+
+
+- (UIDatePicker *) picker {
+  if (_picker != nil) { return _picker; }
+  
+  CGFloat y = 151;
+  CGRect frame = CGRectMake(0, y, 320, 216);
+  UIDatePicker *datePicker = [[UIDatePicker alloc] initWithFrame:frame];
+  datePicker.date = [self.date copy];
+  NSCalendar *calendar = [NSCalendar gregorianCalendarWithMondayAsFirstDayOfWeek];
+
+  datePicker.calendar = calendar;
+  datePicker.maximumDate = nil;
+  datePicker.minimumDate = nil;
+  
+  UIDatePickerMode mode = self.pickerMode;
+  datePicker.datePickerMode =  mode;
+  datePicker.minuteInterval = 1;
+  datePicker.accessibilityIdentifier = [self accessibilityIdentifierForPickerWithMode:mode];
+  [datePicker addTarget:self action:@selector(datePickerDateDidChange:) 
+       forControlEvents:UIControlEventValueChanged];
+  datePicker.tag = kTagPicker;
+  _picker = datePicker;
+  return datePicker;
+}
+
+#pragma mark Actions 
+
+- (void) datePickerDateDidChange:(id)sender {
+  NSLog(@"date picker did change");
+  NSDate *newDate = self.picker.date;
+  NSString *text = [self stringForDate:newDate withMode:self.pickerMode];
+  self.label.text = text;
+}
+
+
+- (void) buttonTouchedPickerDone:(id)sender {
+  NSLog(@"picker done button touched");
+  [self.delegate datePickerViewDoneButtonTouchedWithDate:self.picker.date];
+   
+}
+
+- (NSString *) accessibilityIdentifierForMode:(UIDatePickerMode) aMode {
+  switch (aMode) {
+    case UIDatePickerModeCountDownTimer: return @"count down picker";
+    case UIDatePickerModeDate: return @"date picker";
+    case UIDatePickerModeDateAndTime: return @"date and time picker";
+    case UIDatePickerModeTime: return @"time picker";
+  }
+}
+
+- (NSString *) stringForDate:(NSDate *)aDate withMode:(UIDatePickerMode) aMode {
+
+  NSString *fmt = nil;
+  switch (aMode) {
+    case UIDatePickerModeCountDownTimer: { fmt = @"unknown"; break; }
+    case UIDatePickerModeDate: { fmt = [self stringForDateFormat];  break;}
+    case UIDatePickerModeDateAndTime: { fmt = [self stringForDateTimeFormat]; break;}
+    case UIDatePickerModeTime: { fmt = [self stringForTimeFormat]; break; }
+  }
+
+  NSDateFormatter *df = [self dateFormatterWithFormat:fmt];
+  NSString *str = [df stringFromDate:aDate];
+  NSLog(@"str = '%@'", str);
+  return str;
+}
+
+- (NSString *) accessibilityIdentifierForPickerWithMode:(UIDatePickerMode) aMode {
+  switch (aMode) {
+    case UIDatePickerModeCountDownTimer: return @"count down";
+    case UIDatePickerModeDate: return @"date";
+    case UIDatePickerModeDateAndTime: return @"date and time";
+    case UIDatePickerModeTime: return @"time";
+  }
+}
+
+- (NSString *) stringForDateFormat {
+  NSLocale *locale = [NSLocale autoupdatingCurrentLocale];
+  BOOL uses24h = [locale localeUses24HourClock];
+  NSString *dateFormat = uses24h ? k_24H_date_format : k_12H_date_format;
+  return [NSString stringWithFormat:@"%@", dateFormat];
+}
+
+- (NSString *) stringForDateTimeFormat {
+  NSLocale *locale = [NSLocale autoupdatingCurrentLocale];
+  BOOL uses24h = [locale localeUses24HourClock];
+  NSString *timeFormat = uses24h ? k_24H_time_format : k_12H_time_format;
+  NSString *dateFormat = uses24h ? k_24H_date_format : k_12H_date_format;
+  return [NSString stringWithFormat:@"%@ %@", dateFormat, timeFormat];
+}
+
+-  (NSString *) stringForTimeFormat {
+  NSLocale *locale = [NSLocale autoupdatingCurrentLocale];
+  // 24h use %HH for  00:01, 03:01, 11:01
+  // 24h use %H  for   0:01,  3:01, 11:01
+  // 24h use %k  for  24:01,  3:01, 11:01
+  return [locale localeUses24HourClock] ? k_24H_time_format : k_12H_time_format;
+}
+
+- (NSDateFormatter *) dateFormatterWithFormat:(NSString *) aString {
+  NSDateFormatter *formatter = [NSDateFormatter new];
+  [formatter setDateFormat:aString];
+  formatter.locale = [NSLocale autoupdatingCurrentLocale];
+  formatter.timeZone = [NSTimeZone localTimeZone];
+  [formatter setCalendar:[NSCalendar autoupdatingCurrentCalendar]];
+  return formatter;
+}
+
+
+@end
