75c45f30aa03b6e04bf60553ad86f94e8257fddc
Joshua Moody <joshuajmoody@gmail.com>
2016-09-13 11:36:12 +0200

    Cucumber: simplify the xtc-other-gems.rb

--- CalSmokeApp/config/xtc-other-gems.rb
+++ CalSmokeApp/config/xtc-other-gems.rb
@@ -1,21 +1,11 @@
 gem 'retriable', '~> 2.0'
 gem "cucumber", "~> 2.0"
-gem "xamarin-test-cloud", "2.0.1"
-
-# workflow and build tools
+gem "xamarin-test-cloud", "~> 2.0"
 gem 'rake', '~> 10.3'
 gem 'bundler', '~> 1.6'
 gem 'xcpretty', '~> 0.1'
-
-# rspec stack
 gem 'rspec', '~> 3.0'
-
-# pry
 gem 'pry'
 gem 'pry-nav'
-
-# CI Stack
 gem 'luffa', '~> 1.0', '>= 1.0.2'
-
-# Natural language to date parsing.
 gem 'chronic', '>= 0.10.2', '< 1.0'
