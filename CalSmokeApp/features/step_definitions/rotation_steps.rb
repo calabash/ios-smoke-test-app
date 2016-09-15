6477ccd4e033ba02e3ae009f43d8bc23ca806a14
Joshua Moody <joshuajmoody@gmail.com>
2016-09-15 12:37:47 +0200

    Orientation: fix does-not-rotate for iOS 9 and 10 iPads

--- CalSmokeApp/features/step_definitions/rotation_steps.rb
+++ CalSmokeApp/features/step_definitions/rotation_steps.rb
@@ -4,7 +4,11 @@ And(/^I try to rotate the home button so it is on the (left|right)$/) do |positi
 end
 
 Then(/^the orientation should be portrait$/) do
-  expect(home_direction).to be == :down
+  if ipad? && (ios10? || ios9?)
+    expect(landscape?).to be_truthy
+  else
+    expect(home_direction).to be == :down
+  end
 end
 
 Then(/^the home button should be on the right$/) do
