92804ea93f5ee8981e9dca62d491f32dbf5458fb
Dmitry Shibanov <v-dmshib@microsoft.com>
2019-09-28 14:15:52 +0300

    edit rotation tests

--- CalSmokeApp/features/step_definitions/rotation_steps.rb
+++ CalSmokeApp/features/step_definitions/rotation_steps.rb
@@ -3,7 +3,8 @@ And(/^I try to rotate the home button so it is on the (left|right)$/) do |positi
   rotate_home_button_to position.to_sym
 end
 
-Then(/^the orientation should be portrait$/) do
+Then(/^the orientation should be portrait$/) do |position|
+  rotate_home_button_to position.to_sym
   sleep(3.0)
   if ipad? && (ios11? || ios10? || ios9?)
     expect(landscape?).to be_truthy
@@ -12,7 +13,8 @@ Then(/^the orientation should be portrait$/) do
   end
 end
 
-Then(/^the home button should be on the right$/) do
+Then(/^the home button should be on the right$/) do |position|
+  rotate_home_button_to position.to_sym
   sleep(3.0)
   expect(home_direction).to be == :right
 end
