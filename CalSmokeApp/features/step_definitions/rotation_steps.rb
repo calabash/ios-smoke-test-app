1253585dc52e673f54f8ac244b5a5f1d19c0b022
Dmitry Shibanov <v-dmshib@microsoft.com>
2019-10-01 09:15:08 +0300

    add xcode 10.3 and fix tests

--- CalSmokeApp/features/step_definitions/rotation_steps.rb
+++ CalSmokeApp/features/step_definitions/rotation_steps.rb
@@ -3,19 +3,18 @@ And(/^I try to rotate the home button so it is on the (left|right)$/) do |positi
   rotate_home_button_to position.to_sym
 end
 
-Then(/^the orientation should be portrait$/) do |position|
-  rotate_home_button_to position.to_sym
-  sleep(3.0)
+Then(/^the orientation should be portrait$/) do
   if ipad? && (ios11? || ios10? || ios9?)
     expect(landscape?).to be_truthy
   else
-    expect(home_direction).to be == :down
+    expect(home_direction).to be == :left
   end
 end
 
-Then(/^the home button should be on the right$/) do |position|
+Then(/^the home button should be on the right$/) do
   rotate_home_button_to position.to_sym
   sleep(3.0)
   expect(home_direction).to be == :right
 end
 
+#check these tests
