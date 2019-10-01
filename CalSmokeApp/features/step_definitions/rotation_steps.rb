e97933cb4e9f072a3048eb30437b676dc1277e3c
Dmitry Shibanov <v-dmshib@microsoft.com>
2019-10-01 10:29:12 +0300

    delete position

--- CalSmokeApp/features/step_definitions/rotation_steps.rb
+++ CalSmokeApp/features/step_definitions/rotation_steps.rb
@@ -7,13 +7,11 @@ Then(/^the orientation should be portrait$/) do
   if ipad? && (ios11? || ios10? || ios9?)
     expect(landscape?).to be_truthy
   else
-    expect(home_direction).to be == :left
+    expect(home_direction).to be == :down
   end
 end
 
 Then(/^the home button should be on the right$/) do
-  rotate_home_button_to position.to_sym
-  sleep(3.0)
   expect(home_direction).to be == :right
 end
 
