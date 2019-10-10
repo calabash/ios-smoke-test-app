a528d629a2b2bae317a88fba6aca031fc47f6dc0
Vladimir Safonkin <vsafonkin@gmail.com>
2019-10-10 13:25:00 +0300

    Refactor variables

--- bin/ci/az-publish.sh
+++ bin/ci/az-publish.sh
@@ -28,8 +28,6 @@ function zip_with_ditto {
   info "Installed ${2}"
 }
 
-# Pipeline Variables are set through the AzDevOps UI
-# See also the ./azdevops-pipeline.yml
 if [ -e ./.azure-credentials ]; then
   source ./.azure-credentials
 fi
@@ -49,9 +47,15 @@ if [[ -z "${AZURE_STORAGE_CONNECTION_STRING}" ]]; then
   exit 1
 fi
 
-WORKING_DIR="${BUILD_SOURCESDIRECTORY}"
-PRODUCT_DIR="${WORKING_DIR}/CalSmokeApp/Products/app/CalSmoke-cal"
-INFO_PLIST="${PRODUCT_DIR}/CalSmoke-cal.app/Info.plist"
+if [ "${BUILD_SOURCESDIRECTORY}" != "" ]; then
+  WORKING_DIR="${BUILD_SOURCESDIRECTORY}"
+else
+  WORKING_DIR="."
+fi
+
+APP_PRODUCT_DIR="${WORKING_DIR}/CalSmokeApp/Products/app/CalSmoke-cal"
+IPA_PRODUCT_DIR="${WORKING_DIR}/CalSmokeApp/Products/ipa/CalSmoke-cal"
+INFO_PLIST="${APP_PRODUCT_DIR}/CalSmoke-cal.app/Info.plist"
 
 # Evaluate CalSmokeApp version (from Info.plist)
 VERSION=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" ${INFO_PLIST})
@@ -62,28 +66,26 @@ XC_VERSION=$(/usr/libexec/PlistBuddy -c "Print :DTXcode" ${INFO_PLIST})
 # Evaluate git-sha value
 GIT_SHA=$(git rev-parse --verify HEAD | tr -d '\n')
 
-az --version
-
 # Upload `CalSmokeApp.app` (zipped)
-APP_ZIP="${PRODUCT_DIR}/CalSmoke-cal.app.zip"
-zip_with_ditto "${PRODUCT_DIR}/CalSmoke-cal.app" "${APP_ZIP}"
+APP_ZIP="${APP_PRODUCT_DIR}/CalSmoke-cal.app.zip"
+zip_with_ditto "${APP_PRODUCT_DIR}/CalSmoke-cal.app" "${APP_ZIP}"
 APP_NAME="CalSmoke-${VERSION}-Xcode-${XC_VERSION}-${GIT_SHA}.app.zip"
 azupload "${APP_ZIP}" "${APP_NAME}"
 
 # Upload `CalSmokeApp.app.dSYM` (zipped)
-APP_DSYM_ZIP="${PRODUCT_DIR}/CalSmoke-cal.app.dSYM.zip"
-zip_with_ditto "${PRODUCT_DIR}/CalSmoke-cal.app.dSYM" "${APP_DSYM_ZIP}"
+APP_DSYM_ZIP="${APP_PRODUCT_DIR}/CalSmoke-cal.app.dSYM.zip"
+zip_with_ditto "${APP_PRODUCT_DIR}/CalSmoke-cal.app.dSYM" "${APP_DSYM_ZIP}"
 APP_DSYM_NAME="CalSmoke-${VERSION}-Xcode-${XC_VERSION}-${GIT_SHA}.app.dSYM.zip"
 azupload "${APP_DSYM_ZIP}" "${APP_DSYM_NAME}"
 
 # Upload `CalSmokeApp.ipa`
-IPA="${WORKING_DIR}/CalSmokeApp/Products/ipa/CalSmoke-cal/CalSmoke-cal.ipa"
+IPA="${IPA_PRODUCT_DIR}/CalSmoke-cal.ipa"
 IPA_NAME="CalSmoke-${VERSION}-Xcode-${XC_VERSION}-${GIT_SHA}.ipa"
 azupload "${IPA}" "${IPA_NAME}"
 
 # # Upload `CalSmokeApp.ipa.dSYM` (zipped)
-IPA_DSYM_ZIP="${WORKING_DIR}/CalSmokeApp/Products/ipa/CalSmoke-cal/CalSmoke-cal.app.dSYM.zip"
-zip_with_ditto "${WORKING_DIR}/CalSmokeApp/Products/ipa/CalSmoke-cal/CalSmoke-cal.app.dSYM" "${IPA_DSYM_ZIP}"
+IPA_DSYM_ZIP="${IPA_PRODUCT_DIR}/CalSmoke-cal.app.dSYM.zip"
+zip_with_ditto "${IPA_PRODUCT_DIR}/CalSmoke-cal.app.dSYM" "${IPA_DSYM_ZIP}"
 IPA_DSYM_NAME="CalSmoke-${VERSION}-Xcode-${XC_VERSION}-${GIT_SHA}.ipa.dSYM.zip"
 azupload "${IPA_DSYM_ZIP}" "${IPA_DSYM_NAME}"
 
